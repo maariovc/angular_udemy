b50230cfc0675e33ed27555279b9e10a3850859d
Jesus Mario Vega Carrasco <mario vega@tdg-mbl-hmo199.local>
2020-11-04 08:03:53 -0700

    Section 2 completed.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { getLocaleDateTimeFormat, getLocaleDateFormat } from '@angular/common';
 
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styles: [`.details { color: white; }`]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  allowAddUserName = false;
+  serverCreated = false;
   serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  userName = '';
+  servers = ['Test server', 'Test server 2'];
+  showDetails = false;
+  details = [];
 
   constructor() { 
     setTimeout(() => { 
@@ -21,8 +29,36 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
-    
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
   }
 
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  addUserName() {
+    this.userName = '';
+  }
+  onUserNameValueChanged(event: Event) {
+    if (this.userName != '') {
+      this.allowAddUserName = true;
+    }
+    else {
+      this.allowAddUserName = false;
+    }
+  }
+
+  onDetails(){
+    if (this.showDetails) {
+      this.showDetails = false;
+    } else {
+      this.showDetails = true;
+    }
+    this.details.push(Date.now());
+  }
+
+  getDetailsColor(){
+    return this.details.length > 5 ? "lightBlue" : "none";
+  }
 }
